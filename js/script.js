53b4d8c64964d0073bdca6cb2e44b4ef82fe7e7e
JPgitstuff <jpseaman0001@gmail.com>
2025-07-30 20:54:59 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const MIN_WIDTH_FOR_SCRAMBLE = 460;
+  const MIN_WIDTH_FOR_SCRAMBLE = 350;
   const isWideEnough = window.innerWidth >= MIN_WIDTH_FOR_SCRAMBLE;
 
   // SPLITS THE SKILLS INTO SPANS WITH OPTIONAL SCRAMBLE
